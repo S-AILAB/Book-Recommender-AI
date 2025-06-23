422a285ed7c969474a8c5255184477b59408881b
Siddharth Jain <ai.siddharth73@gmail.com>
2025-06-23 21:40:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,12 +113,12 @@ if st.session_state.get('recommend_triggered', False):
 
     # with col1:
     #     if 
-    st.button("📖Generate Summary")
-    from agents import generate_summary
-    with st.spinner("Generating summary..."):
-                summary = generate_summary(selected_book_title, selected_book_author)
-                st.success("Summary:")
-                st.write(summary)
+    if st.button("📖Generate Summary"):
+                from agents import generate_summary
+                with st.spinner("Generating summary..."):
+                            summary = generate_summary(selected_book_title, selected_book_author)
+                            st.success("Summary:")
+                            st.write(summary)
 
     # with col2:
     #     if st.button("🛒🔗 Get Purchase Links"):
